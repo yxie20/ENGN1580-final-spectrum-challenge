92d9735eb246bd1cf2902236a45111a64bff4b2f
lai-alien-x <yiheng.xie@learnable.ai>
2020-05-15 21:23:05 -0400

    removed some unecessary files

--- Code/Archived/pre5.8_old_Code_w_Albertos_bug/channel.m
+++
@@ -1,8 +0,0 @@
-% Alberto Trovamala
-% ENGN 1580: Channel
-
-% This function simply sums two signals and gaussian noise
-
-function sig = channel(sig_1,sig_2,mu,sig)
-sig = sig_1+sig_2+normrnd(mu,sig);
-end
--- Code/Archived/pre5.8_old_Code_w_Albertos_bug/energ.m
+++
@@ -1,8 +0,0 @@
-% Alberto Trovamala
-% ENGN 1580: Channel
-
-% This function simply sums two signals and gaussian noise
-
-function energy_left = energ(e,sig)
-energy_left = e - (sig^(2));
-end
